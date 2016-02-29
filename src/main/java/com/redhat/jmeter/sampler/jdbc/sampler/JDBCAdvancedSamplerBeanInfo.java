6091be8fe0b083ae9bfad2d14a769b256262dd7e
Vladimir Klevko <vklevko@redhat.com>
2016-02-29 17:59:40 +0100

    Fixing the constructor for the bean info.

--- src/main/java/com/redhat/jmeter/sampler/jdbc/sampler/JDBCAdvancedSamplerBeanInfo.java
+++ src/main/java/com/redhat/jmeter/sampler/jdbc/sampler/JDBCAdvancedSamplerBeanInfo.java
@@ -1,13 +1,16 @@
 package com.redhat.jmeter.sampler.jdbc.sampler;
 
 import com.redhat.jmeter.sampler.jdbc.JDBCAdvancedTestElementBeanInfoSupport;
+import org.apache.jmeter.protocol.jdbc.JDBCTestElementBeanInfoSupport;
 import org.apache.jmeter.testbeans.TestBean;
+
+import java.beans.PropertyDescriptor;
 
 /**
  * Created by vklevko on 2/29/16.
  */
 public class JDBCAdvancedSamplerBeanInfo extends JDBCAdvancedTestElementBeanInfoSupport {
-    public JDBCAdvancedSamplerBeanInfo(Class<? extends TestBean> beanClass) {
-        super(beanClass);
+    public JDBCAdvancedSamplerBeanInfo() {
+        super(JDBCAdvancedSampler.class);
     }
 }
