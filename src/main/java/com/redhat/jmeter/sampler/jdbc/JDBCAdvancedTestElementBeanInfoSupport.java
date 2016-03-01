11f68c7ab65f262a6168dd3d8a16986ed546f002
Vladimir Klevko <vklevko@redhat.com>
2016-03-01 10:40:57 +0100

    Added result set read delay property to the UI.

--- src/main/java/com/redhat/jmeter/sampler/jdbc/JDBCAdvancedTestElementBeanInfoSupport.java
+++ src/main/java/com/redhat/jmeter/sampler/jdbc/JDBCAdvancedTestElementBeanInfoSupport.java
@@ -2,6 +2,7 @@ package com.redhat.jmeter.sampler.jdbc;
 
 import org.apache.jmeter.protocol.jdbc.JDBCTestElementBeanInfoSupport;
 import org.apache.jmeter.testbeans.TestBean;
+import org.apache.jmeter.testbeans.gui.BooleanPropertyEditor;
 
 import java.beans.PropertyDescriptor;
 
@@ -14,10 +15,19 @@ public abstract class JDBCAdvancedTestElementBeanInfoSupport extends JDBCTestEle
         createPropertyGroup("statementConfig",
                 // $NON-NLS-1$
                 new String[]{
-                "fetchSize" // $NON-NLS-1$
-        });
+                        "fetchSize" // $NON-NLS-1$
+                });
+
+        createPropertyGroup("resultSetConfig", // $NON-NLS-1$
+                new String[]{
+                        "resultSetReadDelay" // $NON-NLS-1$
+                });
         PropertyDescriptor p = property("fetchSize"); // $NON-NLS-1$
         p.setValue(NOT_UNDEFINED, Boolean.TRUE);  // $NON-NLS-1$
         p.setValue(DEFAULT, 10);
+
+        p = property("resultSetReadDelay"); // $NON-NLS-1$
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, 0);// $NON-NLS-1$
     }
 }
