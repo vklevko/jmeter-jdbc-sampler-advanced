a6b0606658d7caef15002c996e48032e4b6a8a92
Vladimir Klevko <vklevko@redhat.com>
2016-03-01 09:40:31 +0100

    Removing unused property descriptor setting.

--- src/main/java/com/redhat/jmeter/sampler/jdbc/JDBCAdvancedTestElementBeanInfoSupport.java
+++ src/main/java/com/redhat/jmeter/sampler/jdbc/JDBCAdvancedTestElementBeanInfoSupport.java
@@ -19,7 +19,5 @@ public abstract class JDBCAdvancedTestElementBeanInfoSupport extends JDBCTestEle
         PropertyDescriptor p = property("fetchSize"); // $NON-NLS-1$
         p.setValue(NOT_UNDEFINED, Boolean.TRUE);  // $NON-NLS-1$
         p.setValue(DEFAULT, 10);
-        p.setValue(TEXT_LANGUAGE,"fetchSize");// $NON-NLS-1$
-
     }
 }
